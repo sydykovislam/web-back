443afb44bca405d966564524f10a437a0d786747
sydykovislam <sydykovislam03@gmail.com>
2020-12-21 23:19:51 +0600

    This is a new commit for what I originally planned to be amended

---
+++ shop/alatoo_store/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from apps.products.views import *
+from apps.categories.views import category_detail
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', main_page, name='main-page'),
+    path('<int:pk>/', product_detail, name='product-detail'),
+    path('category/<int:pk>/', category_detail, name='category'),
+    path('cart/', include('apps.cart.urls'))
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
